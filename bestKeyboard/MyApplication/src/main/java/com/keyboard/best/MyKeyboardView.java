2242bdaf73a92aa2d1235e0ecba7b16c31ba4377
AJ <aj.3dev@gmail.com>
2014-01-04 16:29:12 +0100

    bestkeyboard

---
+++ bestKeyboard/MyApplication/src/main/java/com/keyboard/best/MyKeyboardView.java
@@ -0,0 +1,72 @@
+package com.keyboard.best;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+/**
+ * Created by AJ on 03.01.14.
+ */
+public class MyKeyboardView extends RelativeLayout {
+    private final Context mContext;
+    private SoftKeyboard mSoftKeyboard;
+    private Button mbtn1;
+    private Button mbtn2;
+
+    public MyKeyboardView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        mContext = context;
+    }
+
+    public void setSoftKeyboard(SoftKeyboard sk){
+        mSoftKeyboard = sk;
+    }
+
+    public void sendCharacter(String character){
+        if(mSoftKeyboard != null){
+            mSoftKeyboard.sendCharacter(character);
+        }
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        mbtn1 = (Button) findViewById(R.id.btn_1);
+        mbtn1.setOnClickListener(mButtonClickListener);
+
+        mbtn2 = (Button) findViewById(R.id.btn_2);
+        mbtn2.setOnClickListener(mButtonClickListener);
+    }
+
+    private final OnClickListener mButtonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            CharSequence text = "hmm";
+
+            switch (v.getId()) {
+                case R.id.btn_1:
+                    //TOAST
+                    text = "BTN1!";
+                    sendCharacter("a");
+                    break;
+                case R.id.btn_2:
+                    //TOAST
+                    text = "BTN2!";
+                    sendCharacter("b");
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(mContext, text, duration);
+            toast.show();
+        }
+    };
+}
